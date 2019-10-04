5d5d88424d0312b63c5733a18f1e6456999b285c
at670475 <andrea.tabone@broadcom.com>
2019-10-04 09:57:09 +0200

    Init commit

---
+++ src/App/App.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+
+import './App.css';
+import Dashboard from '../dashboard/Dashboard';
+
+class App extends Component {
+  render() {
+    return (
+        <div className="App">
+            <Dashboard></Dashboard>
+        </div>
+    );
+  }
+}
+
+
+
+
+export default App;
---
+++ src/dashboard/Dashboard.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import Text from 'mineral-ui/Text';
+
+export default  class Dashboard extends Component {
+    render() {
+        return (
+            <div>
+                    <Text as="h2">Zowe components</Text>
+            </div>
+        )
+    }
+
+
+}
+
