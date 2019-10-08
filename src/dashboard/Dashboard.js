fd8990e78f20313cd0c406217156f5ae91c83ff2
at670475 <andrea.tabone@broadcom.com>
2019-10-08 09:14:55 +0200

    Add mocked-response for manifesto and mineral-ui library

--- src/dashboard/Dashboard.js
+++ src/dashboard/Dashboard.js
@@ -1,11 +1,39 @@
 import React, { Component } from 'react';
 import Text from 'mineral-ui/Text';
+import Dropdown from 'mineral-ui/Dropdown';
+import Button from 'mineral-ui/Button';
+import Card, { CardBlock, CardTitle} from 'mineral-ui/Card';
 
+let value = require('../mocked-response');
+console.log(value['binaryDependencies']['sourceDependencies'][0]['componentGroup'])
 export default  class Dashboard extends Component {
+
+    getZoweVersion = () => {
+
+        let manifesto = [{
+            text: value["name"],
+        }];
+        return manifesto;
+    };
+
+    getZoweComponents = () => {
+
+
+    }
     render() {
         return (
+
             <div>
-                    <Text as="h2">Zowe components</Text>
+                <Text as="h2">Zowe Components Catalog</Text>
+                <Dropdown data={this.getZoweVersion()}>
+                    <Button>Zowe versions</Button>
+                </Dropdown>
+                <br/>
+                <br/>
+                <Card>
+                    <CardTitle>Card Title</CardTitle>
+                    <CardBlock>{value['binaryDependencies']['sourceDependencies'][0]['componentGroup']}</CardBlock>
+                </Card>
             </div>
         )
     }
